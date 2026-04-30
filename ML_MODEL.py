2a09ca4a0f0524d36467feed69fe258ddbbe30ca
Kengah Ireneaus <4jrkit1@gmail.com>
2026-04-30 08:10:10 +0100

    add documentation on train function

--- ML_MODEL.py
+++ ML_MODEL.py
@@ -2,62 +2,22 @@
 
 
 from dataset import get_all_emails
-from VECTORIZER import text_to_vector, vocabulary
+from VECTORIZER import text_to_vector, build_vocabulary
 
-def dot_product(v1, v2):
-    sum = 0
-    for num1, num2 in zip(v1, v2):
-        sum += num1 * num2
-    return sum
+def dot_product(v1: list, v2: list):
+    """
+    function return the dot product of two vectors
+    """
+    return sum(x * y for x, y in zip(v1, v2))
 
-def train():
+def train() -> tuple[dict, list]:
+    
     emails = get_all_emails()
-    
-    groups = {}
-    
-    label_vectors = {}
-    
-    legitimate_emails =[]
-    phising_emails = []
-    suspicious_emails = []
+    vocab = build_vocabulary()
 
-    prototype = []
-    
-    # group emails by lable
-    for email in emails:
-        if email["label"] == "Legitimate":
-            legitimate_emails.append(email)
-            groups["Legitimate"] = legitimate_emails
-        elif email["label"] == "Suspicious":
-            suspicious_emails.append(email)
-            groups["Suspicious"] = suspicious_emails
-        else:
-            phising_emails.append(email)
-            groups["Phising"] =  phising_emails
-    
-    # covert email to vector
-    for label in groups:
-        label_list = []
-        for email in groups[label]:
-            text = email["subject"].lower() + " " + email["body"].lower()
-            label_list.append(text_to_vector(text, vocabulary))
-        label_vectors[label] = label_list
-    
-    # averaging into one vector prototype
-    for label in label_vectors:
-        prototype_vector = [0] * len(vocabulary)
-        for vector in label_vectors[label]:
-            for i in range(len(vector)):
-                prototype_vector[i] += vector[i]
-        prototype_vector = [x / len(label_vectors[label]) for x in prototype_vector]
-        prototype.append(prototype_vector)
-    
-    # dictionary to store the prototype vector for each label
-    prototype_dict = {}
-    for i, label in enumerate(label_vectors):
-        prototype_dict[label] = prototype[i]
-        print(label)
-    return prototype_dict
+
+
+
 
 def predict(subject, body, prototype_dict, vocab):
     # converting new email to vectors 
